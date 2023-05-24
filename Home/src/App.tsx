7633659586e85a4620024f4b12553b2903597f69
CI5KO <hector_oliva16k@hotmail.com>
2023-05-24 08:06:01 -0600

    Module Federation Tailwind Fix

---
+++ Home/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './main.css'
+
+// @ts-ignore
+import Test from 'user/Test'
+
+function App() {
+  return (
+    <div className="">
+      <div className="bg-red-200">
+        <p className="text-center py-4">This is a text demo</p>
+      </div>
+      <Test />
+    </div>
+  )
+}
+
+export default App
